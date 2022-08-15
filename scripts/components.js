e542edf6022009e2117d00ae31b743319c46dbeb
Trevor <trevorsm@pm.me>
2022-08-14 20:23:03 -0600

    initial files

---
+++ scripts/components.js
@@ -0,0 +1,43 @@
+const Navigation = /*html*/ `
+<nav>
+    <ul>
+        <li>
+        <a href="/">
+            <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-arrow-90deg-left"
+            viewBox="0 0 16 16"
+            >
+            <path
+                fill-rule="evenodd"
+                d="M1.146 4.854a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H12.5A2.5 2.5 0 0 1 15 6.5v8a.5.5 0 0 1-1 0v-8A1.5 1.5 0 0 0 12.5 5H2.707l3.147 3.146a.5.5 0 1 1-.708.708l-4-4z"
+            />
+            </svg>
+            Home
+        </a>
+        </li>
+    </ul>
+</nav>
+`;
+
+const isNotHome = window.location.pathname !== '/';
+
+const Header = /*html*/ `
+<header>
+    <div class="logo">
+        <a href="/">
+            un<span>equip</span>
+        </a>
+    </div>
+    ${isNotHome ? Navigation : ''}
+</header>
+`;
+
+const Footer = /*html*/ `
+<footer>
+    <p class="copy">Copyright &copy; ${new Date().getFullYear()}</p>
+</footer>
+`;
---
+++ scripts/index.js
@@ -0,0 +1,42 @@
+function main() {
+  // This adds the header and footer to the page dynamically.
+  // So there is no need to add them to each page
+  const page = document.querySelector('main');
+  page.innerHTML = /*html*/ `
+    ${Header}
+    <div class="content">
+      ${page.innerHTML}
+    </div>
+    ${Footer}
+  `;
+
+  const nouns = [
+    'optimism',
+    'joy',
+    'hope',
+    'promise',
+    'ambition',
+    'confidence',
+    'attention',
+    'anticipation',
+    'trust',
+    'care',
+    'creativity',
+    'inspiration',
+    'enthusiasm',
+    'happiness',
+    'curiosity',
+    'motivation',
+    'love',
+    'innovation',
+    'change',
+    'adventure',
+    'discovery',
+  ];
+
+  const noun = nouns[Math.floor(Math.random() * nouns.length)];
+  const nounElem = document.querySelector('#noun');
+  if (nounElem) nounElem.innerHTML = noun;
+}
+
+main();
